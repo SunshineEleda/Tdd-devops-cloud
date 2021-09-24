3909f8d7c29987e89ea2aa0a4675f5d9f5d82ad9
Adele Kufour <adele.kufour@news.co.uk>
2021-09-24 08:52:10 +0100

    fizzbuzz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 export const projectName = () => {
-  return 'Kata starter project';
+	return "Kata starter project";
 };
+
+export const fizzBuzz = (num) => {
+	if (num % 5 === 0 && num % 3 === 0) {
+		return "fizzBuzz";
+	}
+	if (num % 3 === 0) {
+		return "fizz";
+	}
+	if (num % 5 === 0) {
+		return "buzz";
+	}
+
+	// multiple of 3 - fizz
+	// multiple of 5 - buzz
+	// multiple of both - fizzbuzz
+
+	return num;
+};
+
+https://github.com/CodingBlackFemales/tdd-js-starter/blob/main/docs/stats-calculator.md
+
+https://github.com/CodingBlackFemales/tdd-js-starter/blob/main/docs/fibonacci-number.md
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,26 @@
-import {projectName} from "./index";
+import { fizzBuzz, projectName } from "./index";
 
-describe('Starter project should', () => {
-  it('output its name', () => {
-    expect(projectName()).toEqual('Kata starter project');
-  });
+describe("Starter project should", () => {
+	it("output its name", () => {
+		expect(projectName()).toEqual("Kata starter project");
+	});
+
+	it("has the correct string length ", () => {
+		const result = projectName();
+
+		expect(result.length).toEqual(20);
+	});
+});
+
+describe("Fizzbuzz", () => {
+	it("should return fizz if the number is a multiple of 3", () => {
+		expect(fizzBuzz(3)).toEqual("fizz");
+	});
+	it("should return buzz if the number is a multiple of 5", () => {
+		expect(fizzBuzz(5)).toEqual("buzz");
+	});
+
+	it("should return fizzbuzz if the number is a multiple of 5 and 3", () => {
+		expect(fizzBuzz(15)).toEqual("fizzBuzz");
+	});
 });
